4661e2e613058cce31ad30f0fb7f849be6b15c79
Fuad <kamilovfuad@gmail.com>
2022-03-27 20:52:46 +0400

    Fill p-admin with necessary conditionals

--- includes/process/p-admin.php
+++ includes/process/p-admin.php
@@ -1,2 +1,24 @@
 <?php
 
+$h = new Helper();
+$msg = '';
+$username = '';
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+
+    if($h->isEmpty(array($username, $_POST['password']))) {
+        $msg = "All field should be filled!";
+    }
+    else {
+        $admin = new Admin($username);
+        if (!$admin->isValidLogin($_POST['password'])) {
+            $msg = "Invalid username or password!";
+        }
+        else {
+            $_SESSION['username'] = $username;
+            $_SESSION['is_admin'] = true;
+            header("Location: write.php");
+        }
+    }
+}
