8a010dcfd1c9f494a992ec4b2a1b6bcdd6e15341
Fuad <kamilovfuad@gmail.com>
2022-03-28 10:55:08 +0400

    Add conditionals to p-write file

--- includes/process/p-write.php
+++ includes/process/p-write.php
@@ -1,2 +1,30 @@
 <?php
+if (!isset($_SESSION['username']) || !isset($_SESSION['is_admin'])) 
+{
+    header("Location: admin.php");
+}
+else 
+{
+    $h = new Helper();
+    $title = '';
+    $post = '';
+    $audience = '';
+    $msg = '';
+    if (isset($_POST['submit'])) 
+    {
+        $title = $_POST['title'];
+        $post = $_POST['post'];
+        $audience = $_POST['audience'];
+    }
 
+    if ($h->isEmpty([$title, $post, $audience])) 
+    {
+        $msg = "All fields are required to be filled";
+    }
+    else 
+    {
+        $admin = new Admin($_SESSION['username']);
+        $admin->insertIntoPostDB($title, $post, $audience);
+        $msg = "Message saved successfully";
+    }
+}
